69f97d9452b85f11e938ebf6e49e0088b2c71cf6
Dave O <38195019+skoopsy@users.noreply.github.com>
2024-11-12 12:13:45 +0000

    Features/06 adding tests (#7)
    * added test for PPGPreProcessor thresholded sections
    
    * changed test naming
    
    * updated github actions workflows
    
    * added scipy to github workflow

---
+++ tests/test_preprocess_ppg.py
@@ -0,0 +1,39 @@
+import pytest
+import pandas as pd
+from datetime import timedelta
+from preprocessors.ppg_preprocess import PPGPreProcessor
+
+@pytest.fixture
+def sample_data():
+    data = pd.DataFrame({
+        'timestamp': pd.date_range(start='2022-01-01 00:00:00', periods=100, freq='1s'),
+        'value': [10, 22, 21, 18, 25, 30, 60, 18, 15, 10] * 10
+    })
+    return data
+
+@pytest.fixture
+def sample_config():
+    return {
+        'ppg_preprocessing': {
+            'threshold': 20,
+            'min_duration': 1
+        },
+        'filter': {
+            'sample_rate': 100,
+            'lowcut': 0.5,
+            'highcut': 10,
+            'order': 4
+        }
+    }
+
+def test_create_thresholded_sections(sample_data, sample_config):
+    preprocessor = PPGPreProcessor(sample_data, sample_config)
+    sections = preprocessor.create_thresholded_sections()
+	
+    #print(sections)
+
+    assert len(sections) == 20
+    assert sections[0]['data_points'].iloc[0] == 2
+    assert sections[1]['data_points'].iloc[0] == 3
+
+#TODO make cheby2 filter check, bit more complicated
