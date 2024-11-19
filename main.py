dee0d445445d7e157225b8f2b3d88e43b7dee5f7
daveo <38195019+skoopsy@users.noreply.github.com>
2024-11-19 17:28:17 +0000

    getting msptd working, need to make smaller scales

--- main.py
+++ main.py
@@ -2,36 +2,41 @@ from src.loaders.config_loader import get_config
 from src.loaders.loader_factory import DataLoaderFactory
 
 from src.preprocessors.ppg_preprocess import PPGPreProcessor
-from src.visuals.plots import plot_ppg_sections_vs_time
+from src.processors.beat_detector_MSPTD import beat_detect_msptd
+from src.visuals.plots import plot_ppg_sections_vs_time, plot_detected_inflections, plot_scaleogram
 
 def main():
-	# Parse cmd line args and load config
-	config = get_config()
+    # Parse cmd line args and load config
+    config = get_config()
 
-	# Extract config params - should make config a struct/obj
-	file_paths = config['data_source']['file_paths']
-	device = config['data_source']['device']
-	sensor_type = config['data_source']['sensor_type']
-	threshold = config['ppg_preprocessing']['threshold']	
+    # Extract config params - should make config a struct/obj
+    file_paths = config['data_source']['file_paths']
+    device = config['data_source']['device']
+    sensor_type = config['data_source']['sensor_type']
+    threshold = config['ppg_preprocessing']['threshold']	
 
-	# Load data
-	loader = DataLoaderFactory.get_loader(device, sensor_type)
-	data = loader.load_data(file_paths)
-	
-	# Preprocess PPG data
-	preprocessor = PPGPreProcessor(data, config)
-	sections = preprocessor.create_thresholded_sections()
-	filtered_sections = preprocessor.filter_cheby2(sections)
-	
-	# Plot entire compliance sections
-	plot_ppg_sections_vs_time(filtered_sections)
-	
-	# Segment into individual pulse waves
-	#beat_detect_msptd()
-	
-	# Run Signal quality indicies
-		
+    # Load data
+    loader = DataLoaderFactory.get_loader(device, sensor_type)
+    data = loader.load_data(file_paths)
+
+    # Preprocess PPG data
+    preprocessor = PPGPreProcessor(data, config)
+    sections = preprocessor.create_thresholded_sections()
+    filtered_sections = preprocessor.filter_cheby2(sections)
+
+    # Plot entire compliance sections
+    #plot_ppg_sections_vs_time(filtered_sections)
+
+    # Segment into individual pulse waves
+    peaks, troughs, maximagram, minimagram = beat_detect_msptd(filtered_sections[10], 'filtered_value',1000)
+    
+    plot_detected_inflections(filtered_sections[0], peaks, troughs)
+    plot_scaleogram(maximagram, 'Local maxima scaleogram')
+    #plot_scaleogram(minimagram, 'Local minima scaleogram')
+
+    # Run Signal quality indicies
+
 
 if __name__ == "__main__":
-	main()
+    main()
 
--- src/processors/beat_detector_MSPTD.py
+++ src/processors/beat_detector_MSPTD.py
@@ -1,72 +1,100 @@
 import numpy as np
+import pandas as pd
+import time
 
-def beat_detect_msptd(data, max_interval=None):
-	"""
-	Detect peaks and troughs in a (quasi-)periodic signal using:
-		- Modified Scholkmann Algorithm
-		- Multi-Scale Peak and Trough Detection
-		- doi: 
+def beat_detect_msptd(data, column, max_interval=None):
+    """
+    Detect peaks and troughs in a (quasi-)periodic signal using:
+        - Modified Scholkmann Algorithm
+        - Multi-Scale Peak and Trough Detection
+        - Uses method based on https://www.doi.org/10.1007/978-3-319-65798-1_39 
 
-	Args:
-		data (numpy.ndarray): Input signal
-		max_interval (int, optional): Max scale to consider for local maxima scaleogram. Defaults to half the signal length
-		
-	Returns:
-		numpy.ndarray: Indicies of detected peaks
-		numpy.ndarray: Indicies of detected troughts
-		numpy.ndarray: Local maxima scalogram
-		numpy.ndarray: Local minima scalogram
-	"""
-	
-	# Input signal length
-	N = len(data)
+    Args:
+        data (numpy.ndarray): Input signal
+        max_interval (int, optional): Max scale to consider for local maxima scaleogram. Defaults to half the signal length
 
-	# Set length for scaleogram
-	if max_interval is None:
-		L = int(np.ceil(N / 2)) - 1
-	else:
-		L = int(np.ceil(max_interval / 2)) - 1
-	
-	# Detrend data by subtracting the mean
-	mean_val = np.nanmean(data)
-	data[np.isnan(data)] = mean_val
-	data = data - np.mean(data)
-	
-	# Initialise local max and min scaleograms
-	Mx = np.zeros((N, L), dtype=bool)
-	Mn = np.zeros((N, L), dtype=bool)
+    Returns:
+        numpy.ndarray: Indicies of detected peaks
+        numpy.ndarray: Indicies of detected troughts
+        numpy.ndarray: Local maxima scalogram
+        numpy.ndarray: Local minima scalogram
+    """
+    start_time = time.time()
+    # Handle dataframe
+    if isinstance(data, pd.DataFrame):
+        # If no col specified
+        if column is None:
+            numeric_columns = data.select_dtypes(include=[np.number]).columns
+            if len(numeric_columns) == 0:
+                raise ValueError("No numeric columns in DataFrame")
+            column = numeric_columns[0]
+        # Extract column as numpy array
+        data = data[column].to_numpy()
 
-	# Compute local maxima scalogram	
-	for scale in range(L):
-		k = scale + 1
-		for i in range(k + 1, N - k + 1):
-			# Check if current value is a local maxima
-			if data[i-1] > data[i - k - 1] and data[i-1] > data[i + k - 1]:
-				Mx[i - 1, scale] = True
-			# Check if current value is a local minima
-			if data[i-1] < data[i - k - 1] and data[i-1] < data[i + k - 1]:	
-				Mn[i - 1, scale] = True
-	
-	maxima_scalogram = Mx
-	minima_scalogram = Mn
+    else:
+        data = np.asarray(data, dtype=float)
 
-	# Find scale with the most maxima and minima
-	# Sums boolean vals in each column of Mx and Mn
-	# Col with the most zeros (true values) corresponds to scale with most max and minima
-	Y = np.sum(Mx, axis=0)
-	d_max = np.argmax(Y)
-	Mx = Mx[:, :d_max + 1]
+    # Mak sure is numpy array and convert to float
+    #data = np.asarray(data, dtype=float)
 
-	Y = np.sum(Mn, axis=0)
-	d_min = np.argmax(Y)
-	Mn = Mn[:, :d_min + 1]
+    # Remove inf
+    data = np.nan_to_num(data, nan=0.0, posinf=0.0, neginf=0.0)
 
-	# Find position of peaks and troughs
-	# Sum non-zero values in each row of Mx and Mn
-	# Rows with a sum of 0 correspond to the position of a peak and trough
-	Zx = np.sum(~Mx, axis=1)
-	Zn = np.sum(~Mn, axis=1)
-	peaks = np.where(Zx == 0)[0]
-	troughs = np.where(Zn == 0)[0]
+    # Input signal length
+    N = len(data)
 
-	return peaks, troughs, maxima_scalogram, minima_scalogram
+    # Set length for scaleogram
+    if max_interval is None:
+        L = int(np.ceil(N / 2)) - 1
+    else:
+        L = int(np.ceil(max_interval / 2)) - 1
+
+    # Detrend data by subtracting the mean
+    #mean_val = np.nanmean(data)
+    #data[np.isnan(data)] = mean_val
+    data = data - np.mean(data)
+
+    # Initialise local max and min scaleograms
+    Mx = np.zeros((N, L), dtype=bool)
+    Mn = np.zeros((N, L), dtype=bool)
+
+    # Compute local maxima scalogram	
+    for scale in range(L):
+        scale_time_start = time.time()
+        k = scale + 1
+        for i in range(k + 1, N - k + 1):
+            # Check if current value is a local maxima
+            if data[i-1] > data[i - k - 1] and data[i-1] > data[i + k - 1]:
+                Mx[i - 1, scale] = True
+            # Check if current value is a local minima
+            if data[i-1] < data[i - k - 1] and data[i-1] < data[i + k - 1]:	
+                Mn[i - 1, scale] = True
+        scale_time_end = time.time()
+        print(f'Time Elapsed: {scale_time_end - scale_time_start}')
+
+    maxima_scalogram = Mx
+    minima_scalogram = Mn
+
+    # Find scale with the most maxima and minima
+    # Sums boolean vals in each column of Mx and Mn
+    # Col with the most zeros (true values) corresponds to scale with most max and minima
+    Y = np.sum(Mx, axis=0)
+    d_max = np.argmax(Y)
+    Mx = Mx[:, :d_max + 1]
+
+    Y = np.sum(Mn, axis=0)
+    d_min = np.argmax(Y)
+    Mn = Mn[:, :d_min + 1]
+
+    # Find position of peaks and troughs
+    # Sum non-zero values in each row of Mx and Mn
+    # Rows with a sum of 0 correspond to the position of a peak and trough
+    Zx = np.sum(~Mx, axis=1)
+    Zn = np.sum(~Mn, axis=1)
+    peaks = np.where(Zx == 0)[0]
+    troughs = np.where(Zn == 0)[0]
+
+    end_time = time.time()
+    overall_time = end_time-start_time
+    print(f'MSPTD Run Time: {overall_time}s')
+    return peaks, troughs, maxima_scalogram, minima_scalogram
