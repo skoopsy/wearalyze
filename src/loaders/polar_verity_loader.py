2412dae7e7f7cfc5d79b28b3f7b22d3fb900d3bb
Dave <38195019+skoopsy@users.noreply.github.com>
2024-12-03 15:32:04 +0000

    added loader_factory test

--- src/loaders/polar_verity_loader.py
+++ src/loaders/polar_verity_loader.py
@@ -2,14 +2,14 @@ import pandas as pd
 from .base_loader import BaseLoader
 
 class PolarVerityLoader(BaseLoader):
-	def load_data(self, file_path):
-		data_frames = []
-		for file_path in file_paths:
-			data = pd.read_csv(file_path)
-			
-			# Change timestamp from ns to ms for standardisation
-		    data['timestamp'] = data['sensor timestamp [ns]']/1000000	
-			data_frames.append(data)
+        def load_data(self, file_path):
+            data_frames = []
+            for file_path in file_paths:
+                data = pd.read_csv(file_path)
 
-		return pd.concat(data_frames, ignore_index=True)
-		
+                # Change timestamp from ns to ms for standardisation
+                data['timestamp'] = data['sensor timestamp [ns]']/1000000	
+                data_frames.append(data)
+
+            return pd.concat(data_frames, ignore_index=True)
+
