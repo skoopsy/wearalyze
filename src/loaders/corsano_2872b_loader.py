25092cd29fefa3422011d80d27f854edbdeaa749
Dave <38195019+skoopsy@users.noreply.github.com>
2024-12-03 17:15:31 +0000

    added test for corsano2872b loader

--- src/loaders/corsano_2872b_loader.py
+++ src/loaders/corsano_2872b_loader.py
@@ -2,14 +2,24 @@ import pandas as pd
 from .base_loader import BaseLoader
 
 class Corsano2872bLoader(BaseLoader):
-	def load_data(self, file_paths):
-		data_frames = []
-		for file_path in file_paths:
-			data = pd.read_csv(file_path)
-			
-			data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
+    def load_data(self, file_paths):
+        data_frames = []
+        required_columns = [
+        "timestamp", "date", "metric_id", "chunk_index",
+        "quality", "body_pose", "led_pd_pos", "offset",
+        "exp", "led", "gain", "value"
+        ]
+        for file_path in file_paths:
+            data = pd.read_csv(file_path)
 
-			data_frames.append(data)
+            # Validate columns
+            if not set(required_columns).issubset(data.columns):
+                raise ValueError(f"File {file_path} is missing required columns. Expected {required_columns}")
 
-		return pd.concat(data_frames, ignore_index=True)
+            # Convert timestamp to datetime
+            data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
+
+            data_frames.append(data)
+
+        return pd.concat(data_frames, ignore_index=True)
 
