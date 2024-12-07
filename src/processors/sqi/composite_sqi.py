4ef9a96cc72ef213a146b05fca94dd452d5902d9
Dave <38195019+skoopsy@users.noreply.github.com>
2024-12-07 18:17:22 +0000

    partially finished composite sqi class

---
+++ src/processors/sqi/composite_sqi.py
@@ -0,0 +1,38 @@
+from src.processors.sqi.base import SQIBase
+
+class CompositeSQI(SQI):
+    def __init__(self, sqi_list, combine_strategy="avg"):
+        """
+        Initialise CompositeSQI with a list of SQIs and a strategy for
+        combining them
+        
+        args:
+            sqi_list (list[SQI]): List of SQI instances
+            combine_strategy (str): How to combine the results
+        """
+        self.sqi_list = sqi_list
+        self.combine_strategy = combine_strategy
+
+    def compute(self, segment):
+        """
+        Compute the composite SQI for a given segment
+
+        args:
+            segment (pd.DataFrame or pd.Series): The segment to perform SQI on
+        
+        returns:
+            float: The combined SQI value. - maybe bool?
+        """
+
+        sqi_results = [sqi.compute(segment) for sqi in self.sqi_list]
+
+        if self.combine_strategy == "average":
+            return sum(sqi_results) / len(sqi_results)
+        #TODO Revisit the output of this when api better understood
+        elif self.combine_strategy == "sequential_boolian_validation"
+            if all(sqi_results) == True:
+                return 1.0 # Think about how to handle bool instead
+            else:
+                return 0.0 # Think about how to handle bool instead
+        else:
+            raise ValueError(f"Unknown combination strategy: {self.combine_strategy}")
--- src/processors/sqi/factory.py
+++ src/processors/sqi/factory.py
@@ -1,9 +1,32 @@
+from src.processors.sqi.composite_sqi import CompositeSQI
 from src.processors.sqi.bpm_plausible import SQIBpmPlausible
 
 class SQIFactory:
     @staticmethod
-    def create_sqi(sqi_type: str):
+    def create_sqi(sqi_type: str, composite_details=None):
+        """
+        Create a Signal Quality Index (SQI) instance based on the type
+        
+        args:
+            sqi_type (str): The type of SQI to create 
+            composite_details (dict): Details for creating a composite SQI.
+        
+        returns:
+            SQI: An instance of the requested SQI type
+        """
         if sqi_type == "bpm_plausible":
             return SQIBpmPlausible()
+
+        elif sqi_type == "composite":
+            if not composite_details or "sqi_types" not in composite_details:
+                raise ValueError(f"Composite SQI requires 'sqi_types' in details.")
+            sqi_list = [
+                SQIFactory.create_sqi(sqi_type=sqi_name)
+                for sqi_name in composite_details["sqi_types"]
+            ]
+            combine_strategy = composite_details.get("combine_strategy")
+            
+            return CompositeSQI(sqi_list, combine_strategy=combine_strategy)
+
         else:
             raise ValueError(f"Unknown SQI: {sqi_type}")
