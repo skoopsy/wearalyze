3423049d9463576e529cb3b4665f0a741bdd4f2f
Dave <38195019+skoopsy@users.noreply.github.com>
2024-12-16 10:52:35 +0000

    .

--- main.py
+++ main.py
@@ -208,8 +208,9 @@ def main():
     # May not need to set as new
     sqi_results = sqi.compute(data)
     
-    """
+    
     sqi_bpms = data[data.sqi_bpm_plausible == True]
+    """
     plt.hist(group_bpms, bins=50, edgecolor='black', alpha=0.7)
     plt.xlabel('BPM')
     plt.ylabel('Frequency')
