a737bbd7ab354944d63140569d0252174cc1ffae
Dave <38195019+skoopsy@users.noreply.github.com>
2024-12-19 15:51:28 +0000

    vectorised ibi_max sqi

--- src/processors/sqi/ibi_max.py
+++ src/processors/sqi/ibi_max.py
@@ -1,4 +1,5 @@
 from src.processors.sqi.base import SQIBase
+import pandas as pd
 
 class SQIIBIMax(SQIBase):
     def compute(self, data):
@@ -8,15 +9,8 @@ class SQIIBIMax(SQIBase):
         min_bpm = 30 # Could dynamically link this to other sqi or config
         max_ibi = 60000 / min_bpm # in miliseconds
         
-        # Check
-        data['sqi_ibi_max'] = data.apply(
-            lambda row: self.max_threshold_check(row, 'ibi_ms', max_ibi), axis=1
-        )
+        data['ibi_max_group'] = pd.to_numeric(data['ibi_max_group'], errors='coerce')
+        
+        data['sqi_ibi_max'] = data['ibi_max_group'] < max_ibi
         
         return data
-
-    def max_threshold_check(self, row, col, threshold):
-        if row[col] == None:
-            return False
-        else:
-            return row[col] < threshold
