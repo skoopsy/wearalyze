dd4b72c4fe609dae621f3c9b61f6c24c4c6b79b5
Dave <38195019+skoopsy@users.noreply.github.com>
2025-02-10 16:10:05 +0000

    refactored polar verity loader

--- src/loaders/polar_verity_loader.py
+++ src/loaders/polar_verity_loader.py
@@ -1,50 +1,77 @@
+import os
+import re
 import pandas as pd
+
 from .base_loader import BaseLoader
 
 class PolarVerityLoader(BaseLoader):
-    def load_data(self, file_paths):
-        """
-        Load data from a list of file paths
-    
-        :params file_path: List of file paths 
-        :return: Concatenated pandas Datafram
-        """
 
-        data_frames = []
+    def __init__(self, config):
+        self.config = config
 
-        #TODO change to be dynamically set by sensors based in configuration
-        required_columns = ["Phone timestamp", "sensor timestamp [ns]",
-                            "channel 0", "channel 1", "channel 2", "ambient"
-                            ] 
-    
-        for file_path in file_paths:
-            data = pd.read_csv(file_path, delimiter=';')
+        # Polar Verity Sense file regex patterns - Polar Sensor Logger (Android)
+        self.sensor_patterns = {
+            "hr": r".*_HR\.txt",
+            "ppg": r".*_PPG\.txt",
+            "acc": r".*_ACC\.txt",
+            "gyro": r".*_GYRO\.txt"
+        }
+
+        # Check correct columns are in the data file
+        self.required_columns = {
+            "hr": ["Phone timestamp", "sensor timestamp [ns]", "HR [bpm]"],
+            "ppg": ["Phone timestamp", "sensor timestamp [ns]", "channel 0", "channel 1", "channel 2", "ambient"],
+            "acc": ["Phone timestamp", "sensor timestamp [ns]", "X [mg]", "Y [mg]", "Z [mg]"],
+            "gyro": ["Phone timestamp", "sensor timestamp [ns]", "X [dps]", "Y [dps]", "Z [dps]"]
+        }
+
+
+    def load_sensor_data(self, sensor, files):
+        """
+        For a sensor type and list of file paths, filter files using device
+        specific regex and load the data into a DataFrame - Combining all files
+        with same sensor type
+        """
         
-            # Check cols
-            if not set(required_columns).issubset(data.columns):
-                raise ValueError(f"File {file_path} is missing required columns. Expected {required_columns}")
+        # Get sensor file names
+        pattern =self.sensor_patterns.get(sensor)
+        if pattern is None:
+            raise ValueError(f"Sensor type not supported / incorrect: {sensor}")
 
-            data_frames.append(data)
+        sensor_files = [f for f in file if re.search(pattern, os.path.basename(f))]
+        if not sensor_files:
+            print(f"PolarVerityLoader.py: No files found for sensor: {sensor}")
+            return pd.DataFrame() # Empty df
 
+        data = []
+        req_cols = self.required_columns{sensor.get(sensor)
+
+        for file_path in sensor_files:
+            file_data = pd.read_csv(file_path, delimiter=";")
+            if not set(req_cols).issubset(file_data.columns):
+                raise ValueError(f"File {file_path} is missing required columns for sensor: {sensor}. Expected {req_cols}")
+            data.append(file_data)
+        
         return pd.concat(data_frames, ignore_index=True)
-
+    
     def standardise(self, data):
         """
         Standardise Polar Verity Sense data
         """
-
-        # Change timestamp from ns to ms for standardisation
-        data['timestamp_ms'] = data['sensor timestamp [ns]']/1000000	
+        
+        if "sensor timestamp [ns]" in data.columns:
+            # Change timestamp from ns to ms for standardisation
+            data['timestamp_ms'] = data['sensor timestamp [ns]']/1000000	
 
         # Column remapping
         data = self._col_name_remap(data)
         
         #TODO This method may need to be stated in config, probably better methods, maybe even kalman. 
         # Avg 3 ppg channels
-        data['ppg'] = data[["ppg_ch0","ppg_ch1", "ppg_ch2"]].mean(axis=1)
+        if sensor == "ppg":
+            data['ppg'] = data[["ppg_ch0","ppg_ch1", "ppg_ch2"]].mean(axis=1)
                 
         #TODO Could remove unused columns here to keep memory lower, for later
-        # data = data[['timestamp_ms','ppg']]
  
         return data
     
