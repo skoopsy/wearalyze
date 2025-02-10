e80de326119284ba1fced271788b96d66f9eab08
Dave <38195019+skoopsy@users.noreply.github.com>
2025-02-10 16:57:19 +0000

    checkpoint_manager refactored

--- src/checkpoints/checkpoint_manager.py
+++ src/checkpoints/checkpoint_manager.py
@@ -4,25 +4,44 @@ import os
 class CheckpointManager:
     """ Encapsulate checkpoint functionality with pickle. """
 
-    def __init__(self, filename: str):
-        self.filename = filename
+    def __init__(self, config: dict):
+        if config:
+            self.save_config = config["checkpoint"]["save"]
+            self.load_config = config["checkpoint"]["load"]
+        else:
+            raise ValueError("Config must be provided")
+
 
     def save(self, data):
-        directory = os.path.dirname(self.filename)
+
+        directory = os.path.dirname(self.save_config.get("directory")
+        data_id = self.save_config.get("data_id")
+        checkpoint_id = self.save_config.get("checkpoint_id")
+        self.fpath = os.path.join(directory, f"{checkpoint_id}_{data_id}.pkl")
+
         if directory and not os.path.exists(directory):
             os.mkdirs(directory, exist_ok=True)
-        with open(self.filename, "wb") as f:
+
+        with open(self.fpath, "wb") as f:
             pickle.dump(data, f)
-        print(f"Checkpoint saved: {self.filename}")
+        print(f"Checkpoint saved: {fpath}")
+
 
     def load(self):
-        if not os.path.exists(self.filename):
-            raise FileNotFoundError(f"Checkpoint file not found: {self.filename}")
-        with open(self.filename, "rb") as f:
+
+        directory = os.path.dirname(self.load_config.get("directory")
+        data_id = self.load_config.get("data_id")
+        checkpoint_id = self.load_config.get("checkpoint_id")
+        self.fpath = os.path.join(directory, f"{checkpoint_id}_{data_id}.pkl")
+
+        if not os.path.exists(fpath):
+            raise FileNotFoundError(f"Checkpoint file not found: {fpath}")
+        with open(self.fpath, "rb") as f:
             data = pickle.load(f)
-        print(f"Checkpoint loaded: {self.filename}")
+
+        print(f"Checkpoint loaded: {fpath}")
         
         return data
 
     def exists(self):
-        return os.path.exists(self.filename)
+        return os.path.exists(self.fpath)
